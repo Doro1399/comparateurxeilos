bca4b757ee416c52227d6b432899668d968c26a7
Doro1399 <julien.sido@gmail.com>
2026-04-29 06:50:36 -0600

    update propfirms

--- lib/xelos-academy-promo.ts
+++ lib/xelos-academy-promo.ts
@@ -8,7 +8,7 @@ const ACADEMY_PROMO_BY_FIRM: Record<string, string> = {
   "Take Profit Trader": "XEILOSTRADING",
   Tradeify: "6923",
   "Funded Next Futures": "TRADING",
-  "Funded Futures Network": "MYFFN40",
+  "Funded Futures Network": "mtdesk",
   "Lucid Trading": "XEILOSTRADING",
   "My Funded Futures": "TPF",
   Blusky: "30OFF",
